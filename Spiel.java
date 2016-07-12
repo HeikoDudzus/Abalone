54109b10825afd772752fe0b58e98e28ffad580a
Heiko Dudzus <heiko.dudzus@gmail.com>
2016-07-12 19:29:31 +0200

    Vektor als eigene Klasse statt innerer Klasse

--- Spiel.java
+++ Spiel.java
@@ -220,41 +220,4 @@ public class Spiel implements Zustand
     public void toConsole() {
         System.out.print(toString());
     }
-
-    public class Vektor {
-        private int dx, dy;
-
-        Vektor()
-        {
-            dx = 0;
-            dy = 0;
-        }
-
-        Vektor(int dx, int dy)
-        {
-            this.dx = dx;
-            this.dy = dy;
-        }
-
-        public int gibX() {
-            return dx;
-        }
-
-        public int gibY() {
-            return dy;
-        }
-
-        public boolean richtungErlaubt() {
-            /* Im Rahmen des Spiels sind nur die Verschiebungen
-             * (1,1), (-1,-1)
-             * (1,0), (-1,0)
-             * (0,1), (0,-1)
-             * erlaubt. Insbesondere sind (1,-1) und (-1,1) verboten, sowie Verschiebungen
-             * um 2 Stellen.
-             */
-            if (dx*dx + dy*dy > 2) return false;
-            if (dx + dy == 0) return false;
-            return true;
-        }
-    }
 }
